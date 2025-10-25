5a7a118f4b6435dedd8e355e3e139e89fda07162
satish <satishkonduru0225@gmail.com>
2025-10-25 06:47:48 +0530

    Static design

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./index.css";
 const initialFriends = [
   {
@@ -21,11 +22,22 @@ const initialFriends = [
 ];
 
 export default function App() {
+  const [showAddFriend, setShowAddFriend] = useState(false);
+
+  function handleToggleAddFriend() {
+    return setShowAddFriend((showAddFriend) => !showAddFriend);
+  }
   return (
     <div className="app">
       <div className="sidebar">
         <FriendsList />
+        {/* <FormAddFriend /> */}
+        {showAddFriend && <FormAddFriend />}
+        <Button onClick={handleToggleAddFriend}>
+          {showAddFriend ? "Close" : "Add Friend"}
+        </Button>
       </div>
+      <FormSplitBill />
     </div>
   );
 }
@@ -59,7 +71,65 @@ function Friend({ friend }) {
       )}
 
       {friend.balance === 0 && <p>You and {friend.name} are even</p>}
-      <button className="button">Select</button>
+      {/* <button className="button">Select</button> */}
+      <Button>Select </Button>
     </li>
+  );
+}
+
+function FormAddFriend() {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("/images/RSK.jpeg");
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    // if (!name || !image) return;
+    const newFriend = {
+      id: crypto.randomUUID(),
+      name,
+      image,
+      balance: 0,
+    };
+    console.log("New Friend: ", newFriend);
+    setName("");
+    setImage("/images/RSK/jpeg");
+  }
+  return (
+    <form className="form-add-friend" onSubmit={handleSubmit}>
+      <label>🧑‍🤝‍🧑 Friend Name: </label>
+      <input type="text" />
+      <label> 🖼️ Image URL</label>
+      <input type="text" />
+      {/* <button className="button">Select</button> */}
+      <Button>Add</Button>
+    </form>
+  );
+}
+
+function Button({ children, onClick }) {
+  return (
+    <button className="button" onClick={onClick}>
+      {children}
+    </button>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split the Bill with X</h2>
+      <label>💰Bill Value: </label>
+      <input type="text" />
+      <label>👦 Your Expense: </label>
+      <input type="text" />
+      <label>🧑‍🤝‍🧑 X's Expense: </label>
+      <input type="text" disabled />
+      <label>🤑 Who is paying the bill?</label>
+      <select>
+        <option value="you">You</option>
+        <option value="friend">X</option>
+      </select>
+      <Button>Split Bill</Button>
+    </form>
   );
 }
