45048672bdd9683d5f04c984c51e1b621453f696
satish <satishkonduru0225@gmail.com>
2025-10-25 05:45:30 +0530

    Spaces adjustment

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ function Friend({ friend }) {
         </p>
       )}
 
-      {friend.balance === 0 && <p>You & {friend.name} are even</p>}
+      {friend.balance === 0 && <p>You and {friend.name} are even</p>}
       <button className="button">Select</button>
     </li>
   );
